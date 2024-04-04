c8da3fea853a0f1b8638f02163844d2075e45c2a
Gabriel Muthenya <gabriel@bildit.co>
2024-04-04 12:11:24 +0300

    update naming

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,8 +7,8 @@ def validUTF8(data):
     """Checks if a list of integers are valid UTF-8 codepoints
     """
     skip = 0
-    n = len(data)
-    for i in range(n):
+    length_data = len(data)
+    for i in range(length_data):
         if skip > 0:
             skip -= 1
             continue
@@ -19,7 +19,7 @@ def validUTF8(data):
         elif data[i] & 0b11111000 == 0b11110000:
             # 4-byte utf-8 character encoding
             span = 4
-            if n - i >= span:
+            if length_data - i >= span:
                 next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
                     data[i + 1: i + span],
@@ -32,7 +32,7 @@ def validUTF8(data):
         elif data[i] & 0b11110000 == 0b11100000:
             # 3-byte utf-8 character encoding
             span = 3
-            if n - i >= span:
+            if length_data - i >= span:
                 next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
                     data[i + 1: i + span],
@@ -45,7 +45,7 @@ def validUTF8(data):
         elif data[i] & 0b11100000 == 0b11000000:
             # 2-byte utf-8 character encoding
             span = 2
-            if n - i >= span:
+            if length_data - i >= span:
                 next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
                     data[i + 1: i + span],
